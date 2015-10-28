9a7e7e3a68655e47c6c778e8efa8bc6453cc426e
raycarnes <ray.carnes@bistasolutions.com>
2015-10-28 16:37:03 -0700

    Initial

---
+++ stock_move_price/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name" : "Stock Move Price",
+    "version" : "1.9.1",
+    "author" : "Bista Solutions",
+    'category': 'Inventory Management',
+    "summary": "Add unit price to Stock Moves Reports (Bista)",
+    'description': "For Odoo Enterprise Version 9.0, this module adds the unit price to the list and form views of Stock Moves.",
+    'maintainer': "Bista Solutions",
+    'website': 'http://www.bistasolutions.com/odoo-implementation-partners',
+    "depends" : ["base","stock"],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "data" : [
+        'views/stock_move.xml',
+    ],
+    "test" : [
+    ],
+    "auto_install": False,
+    "application": False,
+    "installable": True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
