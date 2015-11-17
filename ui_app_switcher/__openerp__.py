1114f53508bb4c68ba8dae7760322da8ebac76e4
raycarnes <ray.carnes@bistasolutions.com>
2015-11-16 16:38:10 -0800

    Resolve bug with blue image not showing up.

--- ui_app_switcher/__openerp__.py
+++ ui_app_switcher/__openerp__.py
@@ -3,7 +3,7 @@
 
 {
     "name" : "Custom App Switcher",
-    "version" : "1.9.1",
+    "version" : "1.9.2",
     "author" : "Bista Solutions",
     "category" : "Extra Tools",
 	"summary": "Change App Switcher background image (Bista)",
