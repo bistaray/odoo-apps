59b713d54d5f9f46937db65d92a0edc991d32154
bistaray <ray.carnes@bistasolutions.com>
2017-01-21 15:26:28 -0800

    Update manifest for Apps Store

--- mail_see_recipients/__manifest__.py
+++ mail_see_recipients/__manifest__.py
@@ -10,6 +10,8 @@
     'version': '1.10.1',
     "author": 'Bista Solutions Pvt. Ltd.',
     'website': 'https://www.odoo.com/page/mrp-cloud-software',
+	'summary': "More intuitive message sending (Bista)",
+    'description': "For Odoo Enterprise Version 9.0, this module replaces the default Apps Switcher background with a single gray image background. By replacing the image file in this module, you can further personalize the Odoo Apps Switcher page. This is a very simply module. It serves as an ideal example for those who are learning how to build modules with Odoo.",	
     'category': 'Mail',
     'sequence': 10,
 	'images': ['static/description/mail_see_recipients.png'],
